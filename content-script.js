0c6a2ae83817cd6a1abe2c31c6b822e0453f72e5
bennyt <bennyt@pop-os.localdomain>
2023-02-19 19:00:36 +0100

    Add highlight functionality

--- content-script.js
+++ content-script.js
@@ -1,54 +1,132 @@
 chrome.runtime.onMessage.addListener(function (request) {
-  const getMatches = (regexString) => {
-    const html = document.getElementsByTagName("body")[0].innerHTML;
-    let m;
+  const html = document.getElementsByTagName("body")[0].innerHTML;
 
+  if (request.highlight !== undefined) {
+    highlightWithTreeWalker(request);
+  } else if (type) {
+    exportValues(request.type, request.regex, html, request.columns);
+  }
+});
+
+let highlightDivs = [];
+
+const getMatches = (regexString, target) => {
+  let match;
+  let matches = [];
+  const regex = new RegExp(regexString, "g");
+
+  while ((match = regex.exec(target)) !== null) {
+    if (match.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+
+    match.forEach((match) => {
+      matches.push(match);
+    });
+  }
+  return matches;
+};
+
+const download = (type, matches) => {
+  let blob = new Blob([matches], {
+      type: "text/" + type,
+    }),
+    a = document.createElement("a");
+
+  a.download = "export." + type;
+  a.href = window.URL.createObjectURL(blob);
+  a.dataset.downloadurl = ["text/" + type, a.download, a.href].join(":");
+  a.click();
+};
+
+const constructCSV = (matches, columns) => {
+  if (!columns) columns = 10;
+  csv = "";
+  matches.forEach((match, index) => {
+    (index + 1) % columns ? (csv += match + ";") : (csv += match + "\n");
+  });
+  return csv;
+};
+
+const exportValues = (type, regexString, target, columns) => {
+  download(type, constructCSV(getMatches(regexString, target), columns));
+};
+
+const highlightWithTreeWalker = (request) => {
+  if (highlightDivs.length > 0) {
+    highlightDivs.forEach((div) => {
+      document.body.removeChild(div);
+    });
+  }
+  highlightDivs = [];
+
+  const treeWalker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    {
+      acceptNode(node) {
+        return NodeFilter.FILTER_ACCEPT;
+      },
+    }
+  );
+
+  let nodeList = [];
+  let currentNode = treeWalker.currentNode;
+
+  while (currentNode) {
+    nodeList.push(currentNode);
+    currentNode = treeWalker.nextNode();
+  }
+
+  const isHidden = (elem) => {
+    const styles = window.getComputedStyle(elem);
+    const rect = elem.getBoundingClientRect(elem);
+    return (
+      styles.display === "none" ||
+      styles.visibility === "hidden" ||
+      rect.width === 0 ||
+      rect.height === 0
+    );
+  };
+
+  nodeList = nodeList
+    .filter((e) => e.nodeType === 3)
+    .filter((e) => !isHidden(e.parentElement));
+
+  nodeList.forEach((node) => {
+    let match;
     let matches = [];
+    const regex = new RegExp(request.highlight, "g");
 
-    const regex = new RegExp(regexString, "g");
-
-    while ((m = regex.exec(html)) !== null) {
-      if (m.index === regex.lastIndex) {
+    while ((match = regex.exec(node.nodeValue)) !== null) {
+      if (match.index === regex.lastIndex) {
         regex.lastIndex++;
       }
+      matches.push(match);
+    }
 
-      m.forEach((match) => {
-        matches.push(match);
+    if (matches.length > 0) {
+      console.log(node, node.parentElement, matches);
+      matches.forEach((match) => {
+        const range = document.createRange();
+        range.setStart(node, match.index);
+        range.setEnd(node, match.index + match[0].length);
+        const rects = range.getClientRects();
+        if (rects.length > 0) {
+          const highlightOverlay = document.createElement("div");
+          const style = `
+            background-color: yellow; 
+            opacity: 0.4; 
+            position: absolute; 
+            height: ${rects["0"].height}px; 
+            width: ${rects["0"].width}px; 
+            left: ${rects["0"].left}px; 
+            top: ${rects["0"].top}px;`;
+          highlightOverlay.style = style;
+          highlightDivs.push(highlightOverlay);
+        }
       });
     }
-    return matches;
-  };
-
-  const constructCSV = (matches, columns) => {
-    if (!columns) columns = 10;
-    csv = "";
-    matches.forEach((match, index) => {
-      (index + 1) % columns ? (csv += match + ";") : (csv += match + "\n");
-    });
-    return csv;
-  };
-
-  download = function (type, matches) {
-    let blob = new Blob([matches], {
-        type: "text/" + type,
-      }),
-      a = document.createElement("a");
-
-    a.download = "export." + type;
-    a.href = window.URL.createObjectURL(blob);
-    a.dataset.downloadurl = ["text/" + type, a.download, a.href].join(":");
-    a.click();
-  };
-
-  const exportValues = (type, regex, columns) => {
-    const matches = getMatches(regex);
-    if (!columns) columns = 10;
-    let formattedMatches = constructCSV(matches, columns);
-    download(type, formattedMatches);
-  };
-
-  const type = request.type ?? "csv";
-  const regex = request.regex ?? "npm";
-  const columns = request.columns ?? 10;
-  exportValues(type, regex, columns);
-});
+  });
+  highlightDivs.forEach((div) => document.body.appendChild(div));
+};
--- popup.js
+++ popup.js
@@ -1,19 +1,26 @@
 window.onload = () => {
-  const sendMessage = async (type) => {
+  const sendMessage = async (message) => {
     const [tab] = await chrome.tabs.query({
       active: true,
       lastFocusedWindow: true,
     });
-    chrome.tabs.sendMessage(tab.id, {
+    chrome.tabs.sendMessage(tab.id, message);
+  };
+  const createDownloadMessage = (type) => {
+    return {
       type: type,
       regex: document.getElementById("regexInput").value,
       columns: document.getElementById("columnsInput").value,
-    });
+    };
   };
   document
     .getElementById("exportAsCSV")
-    .addEventListener("click", () => sendMessage("csv"));
+    .addEventListener("click", () => sendMessage(createDownloadMessage("csv")));
   document
     .getElementById("exportAsTXT")
-    .addEventListener("click", () => sendMessage("txt"));
+    .addEventListener("click", () => sendMessage(createDownloadMessage("txt")));
+  document.getElementById("regexInput").addEventListener("input", (e) => {
+    console.log("change");
+    sendMessage({ highlight: document.getElementById("regexInput").value });
+  });
 };
